787ec8b633f82c0feea57709f9991a60098920e1
liy <liy8272@gmail.com>
2021-11-25 16:24:15 +0000

    Clean up command input
    Add hotkey to show and hide

--- web/ConsoleMananger.ts
+++ web/ConsoleMananger.ts
@@ -14,7 +14,7 @@ class ConsoleManager extends EventEmitter<EventMap> {
 
     this.consoleElement = document.getElementById('console')!;
 
-    const input = document.getElementById('command-input') as HTMLInputElement;
+    const input = document.getElementsByClassName('command-input')[0] as HTMLInputElement;
     input.addEventListener('keydown', async (e: KeyboardEvent) => {
       if (e.key == 'Enter') {
         this.process(input.value);
@@ -28,12 +28,12 @@ class ConsoleManager extends EventEmitter<EventMap> {
       case 'tag':
         const tagView = document.createElement('div', { is: 'tag-view' }) as TagView;
         tagView.update(await tag());
-        this.consoleElement.appendChild(tagView);
+        this.consoleElement.prepend(tagView);
         break;
       case 'branch':
         const branchView = document.createElement('div', { is: 'branch-view' }) as BranchView;
         branchView.update(await allBranches());
-        this.consoleElement.appendChild(branchView);
+        this.consoleElement.prepend(branchView);
         break;
     }
   }
--- web/elements/CommandInput.ts
+++ web/elements/CommandInput.ts
@@ -1,9 +1,24 @@
+import './CommandInput.css';
+
 export class CommandInput extends HTMLInputElement {
   constructor() {
     super();
   }
 
-  connectedCallback() {}
+  connectedCallback() {
+    document.addEventListener('keydown', async (e: KeyboardEvent) => {
+      if (e.key == 'p' && e.ctrlKey) {
+        this.style.display = this.style.display !== 'block' ? 'block' : 'none';
+        this.focus();
+      }
+    });
+
+    this.addEventListener('keydown', async (e: KeyboardEvent) => {
+      if (e.key == 'Enter') {
+        this.style.display = 'none';
+      }
+    });
+  }
 }
 
 customElements.define('command-input', CommandInput, { extends: 'input' });
