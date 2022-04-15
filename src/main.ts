7164e37596a5a6ae6a4e3d1130f92b2bdd0db868
liy <liy8272@gmail.com>
2022-04-15 22:57:57 +0100

    Trying out terra.js

--- src/main.ts
+++ src/main.ts
@@ -138,3 +138,17 @@ if (app.requestSingleInstanceLock()) {
 dialog.showErrorBox = function (title, content) {
   console.log(`${title}\n${content}`);
 };
+
+import { LCDClient, Coin } from '@terra-money/terra.js';
+
+// connect to bombay testnet
+const terra = new LCDClient({
+  URL: 'https://bombay-lcd.terra.dev',
+  chainID: 'bombay-12',
+});
+
+// get the current swap rate from 1 TerraUSD to TerraKRW
+const offerCoin = new Coin('uusd', '1000000');
+terra.market.swapRate(offerCoin, 'ukrw').then((c) => {
+  console.log(`${offerCoin.toString()} can be swapped for ${c.toString()}`);
+});
