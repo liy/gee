dbeeafbbd926a84f56b9fcd59cb73fe1d26e987d
liy <liy8272@gmail.com>
2022-06-16 21:08:36 +0100

    Remove unused old components

--- web/components-old/index.ts
+++
@@ -1,8 +0,0 @@
-import './reference.css';
-
-import './Tag';
-import './Branch';
-import './Commit';
-import './DiffFile';
-import './LogLabel/LogLabel';
-import './HashLink';
--- web/store.ts
+++ web/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { Head } from './commands/log';
 import { Diff } from './Diff';
 import { Actions, PromptAction } from './prompts/actions';
